133925794d1ae492fea825d7cd73436792940265
Yuxuan Huang <yh009@bucknell.edu>
2015-02-02 22:55:46 -0500

    Lab2.2 completed

---
+++ Lab2/Lab2/music1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "snode.h"
+#include "slist.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+
+int main(int argc, char *argv[]) {
+  char *prompt_str = "enter song title:";
+  char *song_title;
+  struct slist *the_slist1 = slist_create();
+  printf("Testing add_back\n");
+  while (true) {
+             song_title = readline(prompt_str);
+	     if (song_title == NULL){
+	       break;
+	     }
+	     else{
+	       slist_add_back(the_slist1,song_title);
+	     }
+    }
+  printf("\n");
+  slist_traverse(the_slist1);
+
+  struct slist* the_slist2 = slist_create();
+  printf("Testing add_front\n");
+  while (true) {
+             song_title = readline(prompt_str);
+             if (song_title == NULL){
+	       break;
+	     }
+	     else{
+	       slist_add_front(the_slist2,song_title);
+	     }
+    }
+  printf("\n");
+  slist_traverse(the_slist2);
+  return 0;
+
+}
+
+
