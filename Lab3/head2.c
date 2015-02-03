391a0bf0e2e63208a437bb1423ddc7f6e3377d03
Yuxuan Huang <yh009@bucknell.edu>
2015-02-03 03:32:02 -0500

    prelab3,exercise 2 complete

---
+++ Lab3/head2.c
@@ -0,0 +1,18 @@
+//Yuxuan Huang
+//Lab3
+//1pm
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	FILE *file;
+	file = fopen(argv[1], "r");
+	char *buf;
+	buf = malloc(sizeof(char) * 4096);
+	int a;
+	for (a = 0; a < 10; ++a){
+		fgets(buf, 4096, file);
+		printf("%s", buf);
+	}
+}
