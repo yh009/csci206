9b2ddfa932f7b465278bae9249e5c7c5397d0e6a
Yuxuan Huang <yh009@bucknell.edu>
2015-02-09 22:47:25 -0500

    lab3 exercise 1 complete

--- Lab3/head2.c
+++ Lab3/head2.c
@@ -9,10 +9,11 @@ int main(int argc, char *argv[]){
 	FILE *file;
 	file = fopen(argv[1], "r");
 	char *buf;
-	buf = malloc(sizeof(char) * 4096);
+	buf = malloc(sizeof(char) * 5000);
 	int a;
 	for (a = 0; a < 10; ++a){
-		fgets(buf, 4096, file);
+		fgets(buf, 5000, file);
 		printf("%s", buf);
 	}
+	return 0;
 }
